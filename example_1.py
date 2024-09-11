f02e8e0250bb4b73ecd568bcaa5b4b191d01f893
«Anna» <anjakabanova@yandex.ru>
2024-09-11 15:38:30 +0300

    example_1

---
+++ example_1.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://example.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Извлечение заголовка
+title = soup.title.string
+print(title)
